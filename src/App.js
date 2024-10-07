d77e7886045cafbd5395eb2808b48f81e54c130b
e2dubba <svirgilgoodwin@gmail.com>
2024-10-07 07:28:07 -0600

    Create Initial Presentation
    * Updating the packages
    * Create a markdown file and read it into slides
    * Initial basic control

--- src/App.js
+++ src/App.js
@@ -1,26 +1,75 @@
-import logo from "./logo.svg";
 import "./App.css";
 
+import React, { useEffect, useState } from "react";
+
+import {
+  Deck,
+  Slide,
+  Markdown,
+  Heading,
+  DefaultTemplate,
+  MarkdownSlide,
+  MarkdownSlideSet,
+} from "spectacle";
+
+//import raw from "data/ConstraintsFroLinkedOpenData.md";
+import raw from "./data/ConstraintsForLinkedOpenData.md";
+
+const mdText =
+  "# Use Markdown to write a slide\n This is a single slide composed using Markdown.\n - It uses the `animateListItems` prop so...\n - it's list items... \n - will animate in, one at a time.\n";
+
 function App() {
+  const [md, setMd] = useState("");
+  useEffect(() => {
+    fetch(raw)
+      .then((r) => r.text())
+      .then((text) => setMd(text));
+    console.log(typeof md);
+  }, []);
+  // if (md !== "") {
+  //   let txtArray = md.split("\n#"); //.map((txt, idx) => {
+  //   console.log(txtArray);
+  //   //});
+  // }
+  if (md === "") {
+    return <div>Building</div>;
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>Hello World!</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Deck template={<DefaultTemplate />}>
+      <MarkdownSlideSet>{md}</MarkdownSlideSet>
+    </Deck>
   );
 }
 
 export default App;
+
+/*
+      <Slide>
+        <Heading>Welcome to Spectacle</Heading>
+      </Slide>
+      <Slide>
+        <Heading>This is a test</Heading>
+        <ul>
+          <li>One</li>
+          <li>Two</li>
+        </ul>
+      </Slide>
+      {md != "" &&
+        md.split("\n#").map((slideText) => {
+          console.log(slideText);
+          return (
+            <Slide>
+              <Markdown>{slideText}</Markdown>
+            </Slide>
+          );
+        })}
+
+         {md != "" &&
+        md
+          .split("\n#")
+          .filter((txt) => txt != "")
+        .map((slideText) => {
+          console.log(slideText);
+          return <MarkdownSlide animateListItems> {slideText}</MarkdownSlide>;
+        })}
+  */
