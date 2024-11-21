796f0476131d4fdca1faba6d354b1ec4129663d0
e2dubba <svirgilgoodwin@gmail.com>
2024-11-21 11:20:43 -0700

    add theme for sources

---
+++ src/themes/constraining_theme.js
@@ -0,0 +1,38 @@
+const constrainingTheme = {
+  size: {
+    width: 1366,
+    height: 768,
+    maxCodePaneHeight: 200,
+  },
+  colors: {
+    // primary: "#ebe5da",
+    // secondary: "#fc6986",
+    // tertiary: "#1e2852",
+    // quaternary: "#ffc951",
+    // quinary: "#8bddfd",
+    primary: "#01161E",
+    // secondary: "#124559", // midnight green
+    secondary: "#EFF6E0",
+    tertiary: "#598392", // Airforce blue
+    //tertiary: "#AEC3B0",
+    //quaternary: "#598392",
+    quaternary: "#AEC3B0", // Ash grey
+    // quinary: "#EFF6E0", // Beige
+    quinary: "#124559",
+  },
+  fonts: {
+    header: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    text: '"Helvetica Neue", Helvetica, Arial, sans-serif',
+    monospace: '"Consolas", "Menlo", monospace',
+  },
+  fontSizes: {
+    h1: "72px",
+    h2: "64px",
+    h3: "56px",
+    text: "44px",
+    monospace: "20px",
+  },
+  space: [16, 24, 32],
+};
+
+export default constrainingTheme;
